d3bb6489de2cb2b17299ffb2f620a15ede5d28bc
Brandonwashere <80346420+brandon-cle@users.noreply.github.com>
2023-05-24 21:53:53 -0600

    movement test 2

--- panel_de_partidos.js
+++ panel_de_partidos.js
@@ -1,6 +1,6 @@
 const carousel = document.querySelector('#panel_de_partidos_vert');
 const carouselContainer = document.querySelector('.carrusel_de_partidos');
-const carouselItems = document.querySelector('slide');
+const carouselItems = document.querySelector('.slide');
 
 //Variables de control
 let currentIndex = 0;
