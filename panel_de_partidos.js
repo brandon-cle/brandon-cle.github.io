4de7264ba85d5a29579b607137af8d8470465778
Brandonwashere <80346420+brandon-cle@users.noreply.github.com>
2023-05-24 22:45:37 -0600

    Test movement 3

--- panel_de_partidos.js
+++ panel_de_partidos.js
@@ -1,26 +1,27 @@
-const carousel = document.querySelector('#panel_de_partidos_vert');
+
+const carousel = document.querySelector('.panel_de_partidos_vert');
 const carouselContainer = document.querySelector('.carrusel_de_partidos');
-const carouselItems = document.querySelector('.slide');
+const carouselItems = document.querySelectorAll('.slide');
 
 //Variables de control
 let currentIndex = 0;
 const itemWidth = carouselItems[0].offsetWidth;
 
 function moveToNext(){
-    if(currentIndex < carouselItems.lenght - 1){
+    if(currentIndex < carouselItems.length - 1){
         currentIndex++;
-        carouselContainer.style.transform = 'translateX(-${currentIndex * itemWidth}px)';
+        carouselContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
 
     }
 }
 
 function moveToPrev(){
     if(currentIndex > 0){
-        currentIndex++;
-        carouselContainer.style.transform = 'translateX(-${currentIndex * itemWidth}px)';
+        currentIndex--;
+        carouselContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
     }
 }
 
-document.querySelector('#LEFT_btn').addEventListener('click', moveToNext);
-document.querySelector('#RIGHT_btn').addEventListener('click', moveToPrev);
+document.querySelector('#RIGHT_btn').addEventListener('click', moveToNext);
+document.querySelector('#LEFT_btn').addEventListener('click', moveToPrev);
 
