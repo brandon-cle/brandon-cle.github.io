d8c072fdc318d92ed3ea0ff7a8edcd12d3d21753
Brandonwashere <80346420+brandon-cle@users.noreply.github.com>
2023-06-16 11:37:25 -0600

    YAAAAAAAAAAA

---
+++ secciones/personalización/step_script.js
@@ -0,0 +1,19 @@
+function redirectToPage() {
+  window.location.href = "/secciones/registrarse.html";
+}
+
+function redirectToStep1() {
+  window.location.href = "/secciones/personalización/step_by_step.html";
+}
+
+function redirectToStep2() {
+  window.location.href = "/secciones/personalización/2step_by_step.html";
+}
+
+function redirectToStep3() {
+  window.location.href = "/secciones/personalización/3step_by_step.html";
+}
+
+function redirectToStepFinal() {
+  window.location.href = "/secciones/personalización/final_step.html";
+}
